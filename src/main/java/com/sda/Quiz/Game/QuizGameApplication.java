4ead009dbe8094a37111725862ba4acc276c1e5d
Veiko Järvet <jarvet.veiko@gmail.com>
2023-06-26 11:28:18 +0300

    Initial commit

---
+++ src/main/java/com/sda/Quiz/Game/QuizGameApplication.java
@@ -0,0 +1,13 @@
+package com.sda.Quiz.Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGameApplication.class, args);
+	}
+
+}
